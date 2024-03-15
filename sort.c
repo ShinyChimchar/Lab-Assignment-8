5b442183e4bc5fd17916bb9b1c993e1fa3545341
Joshua Titre <joshuatitre@gmail.com>
2024-03-15 16:49:48 -0400

    merge function added and mergeSort completed.

--- sort.c
+++ sort.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int extraMemoryAllocated;
 
-void *Alloc(size_t sz)
+void* Alloc(size_t sz)
 {
 	extraMemoryAllocated += sz;
 	size_t* ret = malloc(sizeof(size_t) + sz);
@@ -29,55 +30,115 @@ size_t Size(void* ptr)
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
+
+// merge function
+void merge(int arr[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	// Create temp arrays 
+	int* L = (int*)Alloc(n1*sizeof(int));
+	int* R = (int*)Alloc(n2 * sizeof(int));
+	
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+
+	i = 0;
+	j = 0;
+	k = l;
+
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+
+	// free memory
+	DeAlloc(L);
+	DeAlloc(R);
+}
+
 void mergeSort(int pData[], int l, int r)
 {
+	if (l < r)
+	{
+		int m = (l + r) / 2;
+		
+		mergeSort(pData, l, m);
+		mergeSort(pData, m + 1, r);
+		merge(pData, l, m, r);
+	}
 }
 
 // parses input file to an integer array
-int parseData(char *inputFileName, int **ppData)
+int parseData(char* inputFileName, int** ppData)
 {
-	FILE* inFile = fopen(inputFileName,"r");
+	FILE* inFile = fopen(inputFileName, "r");
 	int dataSz = 0;
-	int i, n, *data;
+	int i, n, * data;
 	*ppData = NULL;
-	
+
 	if (inFile)
 	{
-		fscanf(inFile,"%d\n",&dataSz);
-		*ppData = (int *)malloc(sizeof(int) * dataSz);
+		fscanf(inFile, "%d\n", &dataSz);
+		*ppData = (int*)malloc(sizeof(int) * dataSz);
 		// Implement parse data block
 		if (*ppData == NULL)
 		{
 			printf("Cannot allocate memory\n");
 			exit(-1);
 		}
-		for (i=0;i<dataSz;++i)
+		for (i = 0; i < dataSz; ++i)
 		{
-			fscanf(inFile, "%d ",&n);
+			fscanf(inFile, "%d ", &n);
 			data = *ppData + i;
 			*data = n;
 		}
 
 		fclose(inFile);
 	}
-	
+
 	return dataSz;
 }
 
 // prints first and last 100 items in the data array
+// Fix printArray 
 void printArray(int pData[], int dataSz)
 {
 	int i, sz = dataSz - 100;
 	printf("\tData:\n\t");
-	for (i=0;i<100;++i)
+	for (i = 0; i < 100; ++i)
 	{
-		printf("%d ",pData[i]);
+		printf("%d ", pData[i]);
 	}
 	printf("\n\t");
-	
-	for (i=sz;i<dataSz;++i)
+
+	sz = dataSz - 100 > 0 ? dataSz - 100 : 0;
+	for (i = sz; i < dataSz; ++i)
 	{
-		printf("%d ",pData[i]);
+		printf("%d ", pData[i]);
 	}
 	printf("\n\n");
 }
@@ -86,36 +147,36 @@ int main(void)
 {
 	clock_t start, end;
 	int i;
-    double cpu_time_used;
+	double cpu_time_used;
 	char* fileNames[] = { "input1.txt", "input2.txt", "input3.txt", "input4.txt" };
-	
-	for (i=0;i<4;++i)
+
+	for (i = 0; i < 4; ++i)
 	{
-		int *pDataSrc, *pDataCopy;
+		int* pDataSrc, * pDataCopy;
 		int dataSz = parseData(fileNames[i], &pDataSrc);
-		
+
 		if (dataSz <= 0)
 			continue;
-		
-		pDataCopy = (int *)malloc(sizeof(int)*dataSz);
-	
+
+		pDataCopy = (int*)malloc(sizeof(int) * dataSz);
+
 		printf("---------------------------\n");
-		printf("Dataset Size : %d\n",dataSz);
+		printf("Dataset Size : %d\n", dataSz);
 		printf("---------------------------\n");
-		
+
 		printf("Merge Sort:\n");
-		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
+		memcpy(pDataCopy, pDataSrc, dataSz * sizeof(int));
 		extraMemoryAllocated = 0;
 		start = clock();
 		mergeSort(pDataCopy, 0, dataSz - 1);
 		end = clock();
-		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
-		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
+		cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+		printf("\truntime\t\t\t: %.1lf\n", cpu_time_used);
+		printf("\textra memory allocated\t: %d\n", extraMemoryAllocated);
 		printArray(pDataCopy, dataSz);
-		
+
 		free(pDataCopy);
 		free(pDataSrc);
 	}
-	
+
 }
